5cd678d87aa3e2fe082abd1b5d6c0b2f4b22b018
Tester0116 <nursultan-051202@mail.ru>
2023-05-16 19:16:14 +0300

    first commit

---
+++ tg_bot.py
@@ -0,0 +1,35 @@
+import openai
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+openai.api_key = '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'
+token = '6102665567:AAEkMacrJNrMPC0Hk99UOQDl0DGFWioVwIA'
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+def update(messages, role, content):
+    messages.append({"role":role, "content":content})
+    return messages
+
+# обработчик команды /start
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет, я бот! Я могу отвечать на ваши сообщения.")
+
+@dp.message_handler()
+async def send(message : types.Message):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system","content":"You are a helpful assistant"},
+        {"role":"user","content":"/start"},
+        {"role":"assistant","content":"Hello Nurick"},
+        {"role":"user","content":message.text},
+    ]
+)
+
+    await message.answer(response['choices'][0]['message']['content'])
+
+executor.start_polling(dp, skip_updates=True)
